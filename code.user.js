0db2f4f9493812ff3f533e48337495ffa9c493b0
imadraude <imadraude777@gmail.com>
2025-12-26 15:15:33 +0200

    Update version to 7.1.29

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/imadraude
 // @author       imadraude
 // @license      MIT
-// @version      7.1.25
+// @version      7.1.29
 // @description  Enhances the Steam Inventory and Steam Market.
 // @match        https://steamcommunity.com/id/*/inventory*
 // @match        https://steamcommunity.com/profiles/*/inventory*
